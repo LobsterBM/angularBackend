f25a53dac7ccc47628ec2a09e81882f3b1f91c49
Omar Kraim <omar.kraim@student.uclouvain.be>
2022-04-03 23:37:01 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -25,6 +25,16 @@ app.use(express.urlencoded({extended: false}));
 
 app.use(`/`, router);
 
+//use cors
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+}
+);
+
+
+
 
 
 
